c5392052a2e4c3b14134e41d8a05eca7c8f98f89
Matheus Geres <matheusgeres@gmail.com>
2025-02-09 21:34:56 -0300

    fix: update website URL in configuration

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://matheusgeres.github.io", // replace this with your deployed domain
+  website: "https://matheusgeres.github.io/geresdev-astro", // replace this with your deployed domain
   base: 'geresdev-astro',
   author: "Sat Naing",
   profile: "https://satnaing.dev/",
